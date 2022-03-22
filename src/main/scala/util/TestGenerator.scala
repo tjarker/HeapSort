8521c5ffd42f605df147ca16c866b26736b85a5c
Tjark Petersen <tjark-petersen@gmx.de>
2022-03-22 08:57:58 +0100

    Update TestGenerator.scala

--- src/main/scala/util/TestGenerator.scala
+++ src/main/scala/util/TestGenerator.scala
@@ -8,15 +8,10 @@ object TestGenerator extends App {
   val w = args(args.indexOf("-w") + 1).toInt
   val target = args(args.indexOf("-o") + 1)
 
-  val testSeq = Seq.tabulate(n)(i => BigInt(i + 1)).reverse //Seq.fill(n)(BigInt(w-1, scala.util.Random)+6)
+  val testSeq = Seq.fill(n)(BigInt(w-1, scala.util.Random) + 0xF)
 
-  println(s"Output: $target\nnr. of values: $n\nwidth: $w")
+  println(s"Output: $target\nnr. of values: $n\nwidth: $w\nfirst element: ${testSeq.head}\nmin: ${testSeq.min}\nmax: ${testSeq.max}")
 
   writeHexSeqToFile(testSeq, target)
 
-}
-object GetTestMinimum extends App {
-  val source = Source.fromFile(args.head)
-  val testSeq = source.getLines().map(BigInt(_, 16)).toArray
-  println(s"Test file: ${args.head}\nMinimum: ${testSeq.min}\nExpected output: 0b${(testSeq.min & 0x7FF).toString(2).reverse.padTo(15,"0").reverse.mkString("")}")
 }
